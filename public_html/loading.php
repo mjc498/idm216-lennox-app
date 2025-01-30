9a86e1858cfc8b3b440d433b5d4bd539f522714e
Maria-Louisa Ching <146354776+mjc498@users.noreply.github.com>
2025-01-29 22:21:02 -0500

    🤍 login cleaup

--- public_html/loading.php
+++ public_html/loading.php
@@ -7,12 +7,19 @@ require '../config.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loading</title>
-    <link rel="stylesheet" href="css/loading.css">
-</head>
 
-    <body style="background-image: url(images/login-pattern.svg);">
-    
+<!-- LINKS -->
+    <link rel="stylesheet" href="css/loading.css">
+    <link rel="stylesheet" href="fonts/font-face.css">
+
+<!-- FAVICON -->
+    <link rel="icon" type="image/x-icon" href="images/lennox_white.png">
+
+    <title>Login</title>
+</head>
+<body style="background-image: url(images/login-pattern.svg);">
+
+<!-- LOGIN CONTAINER -->
     <div class="login-container">
         <h1>LENNOX</h1><h2>GOT LUNCH</h2>
         <p class="login-text">Email</p>
