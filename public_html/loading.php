0828a77348db0091513ea51d4c32e2aa240b1d9c
Maria-Louisa Ching <146354776+mjc498@users.noreply.github.com>
2025-01-29 19:44:47 -0500

    🤍 loading/login/signup

---
+++ public_html/loading.php
@@ -0,0 +1,26 @@
+<?php 
+require '../config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loading</title>
+    <link rel="stylesheet" href="css/loading.css">
+</head>
+
+    <body style="background-image: url(images/login-pattern.svg);">
+    
+    <div class="login-container">
+        <h1>LENNOX</h1><h2>GOT LUNCH</h2>
+        <p class="login-text">Email</p>
+        <input type="email" placeholder="useremailaddress@gmail.com" required>
+        <p class="login-text">Password</p>
+        <input type="password" placeholder="******************" required>
+        <button class="login-button">Login</button>
+    </div>
+
+    </body>
+</html>
