2d7e0c75d5c877eeb11e8562befee1e6ab3275df
Maria-Louisa Ching <146354776+mjc498@users.noreply.github.com>
2025-02-01 17:17:47 -0500

    🤍 Update login.php
    ⭐️ copy and paste from HTML file
    ✅ ready for PHP

--- public_html/login.php
+++ public_html/login.php
@@ -1,33 +1,3 @@
 <?php 
 require '../config.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<!-- LINKS -->
-    <link rel="stylesheet" href="css/loading.css">
-    <link rel="stylesheet" href="fonts/font-face.css">
-
-<!-- FAVICON -->
-    <link rel="icon" type="image/x-icon" href="images/lennox_white.png">
-
-    <title>Login</title>
-</head>
-<body style="background-image: url(images/login-pattern.svg);">
-
-<!-- LOGIN CONTAINER -->
-    <div class="login-container">
-        <h1>LENNOX</h1><h2>GOT LUNCH</h2>
-        <p class="login-text">Email</p>
-        <input type="email" placeholder="useremailaddress@gmail.com" required>
-        <p class="login-text">Password</p>
-        <input type="password" placeholder="******************" required>
-        <button class="login-button">Login</button>
-    </div>
-
-    </body>
-</html>
