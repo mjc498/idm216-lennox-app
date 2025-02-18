430bda4b4762d030931fab6f2a7496d19c772d09
Zoey Bennett <zkb25@drexel.edu>
2025-02-18 08:20:21 -0500

    fix2

--- public_html/landing.php
+++ public_html/landing.php
@@ -1,24 +1,22 @@
 <?php 
 require '../config.php';
 ?>
-
 <!DOCTYPE html>
-<!-- Landing PAGE -->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!-- LINKS -->
-    <link rel="stylesheet" href="css/loading.css">
-    <link rel="stylesheet" href="fonts/font-face.css">
+    <link rel="stylesheet" href="../css/loading.css">
+    <link rel="stylesheet" href="../fonts/font-face.css">
 
 <!-- FAVICON -->
-    <link rel="icon" type="image/x-icon" href="images/logo-lennox.png">
+    <link rel="icon" type="image/x-icon" href="../images/logo-lennox.png">
 
     <title>Landing</title>
 </head>
-<body style="background-image: url(images/background-pattern.svg);">
+<body style="background-image: url(../images/background-pattern.svg);">
 
 <!-- LOGIN & SIGNUP CONTAINER | ADD PHP -->
     <div class="login-container">
@@ -35,10 +33,10 @@ require '../config.php';
             <button type="submit" class="signup-button">Sign up</button>  <!-- ADD PHP FOR SIGN UP -->
         </a>
 
-    <!-- GUEST OPTION | ADD PHP VISIT LATER-->
+    <!-- GUEST OPTION | ADD PHP -->
         <section class="guest-option">
             <p>or 
-                <a href="guesthome.php" style="font-style: normal; color: var(--accent-color-two);">
+                <a href="./index.html" style="font-style: normal; color: var(--accent-color-two);">
                     <i>Continue as Guest</i>
                 </a>
             </p>
@@ -61,7 +59,7 @@ require '../config.php';
     document.addEventListener("DOMContentLoaded", function () {
         if (!sessionStorage.getItem("hasSeenLoading")) {
             sessionStorage.setItem("hasSeenLoading", "true");
-            window.location.href = "loading.html";
+            window.location.href = "loading.php";
         } else {
             document.body.style.opacity = "0";
             document.body.style.transition = "opacity 0.5s ease-in-out";
